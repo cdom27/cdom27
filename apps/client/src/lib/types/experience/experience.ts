4b4b3cacaab42bee7d5745930e07fc847f362a44
Cristian <c.isaacdominguez@gmail.com>
2025-07-14 13:08:47 -0700

    create experience card and type, update all affected files

---
+++ apps/client/src/lib/types/experience/experience.ts
@@ -0,0 +1,7 @@
+export type Experience = {
+  company: string;
+  title: string;
+  start: string;
+  end: string;
+  responsibilities: string[];
+};
--- apps/client/src/routes/landing.tsx
+++ apps/client/src/routes/landing.tsx
@@ -13,6 +13,7 @@ import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/counter.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 import 'yet-another-react-lightbox/plugins/captions.css';
+import ExperienceCard from '../components/ui/display-cards/experience-card/experience-card';
 
 const delayClasses = [
   'motion-delay-[600ms]',
@@ -226,36 +227,11 @@ const Landing = () => {
 
           {EXP.map((exp) => {
             return (
-              <article key={exp.company} className='max-w-[525px]'>
-                <div className='flex items-center gap-2'>
-                  <div
-                    className='bg-bg-dark min-w-4 size-4 intersect-once intersect:motion-preset-fade motion-delay-300 motion-duration-1000'
-                    aria-hidden='true'
-                    role='presentation'
-                  />
-                  <h4 className='text-xl sm:text-2xl font-medium intersect-once intersect:motion-preset-fade motion-delay-[400ms] motion-duration-1000'>
-                    {exp.company}
-                  </h4>
-                </div>
-
-                <p className='pl-6 intersect-once intersect:motion-preset-fade motion-delay-[500ms] motion-duration-1000'>
-                  {exp.title} <br /> {exp.start} - {exp.end}
-                </p>
-                <ul className='list-disc pl-10 mt-2'>
-                  {exp.responsibilities.map((resp, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className={`intersect-once intersect:motion-preset-fade motion-duration-1000 ${
-                          delayClasses[index] || 'motion-delay-[900ms]'
-                        }`}
-                      >
-                        {resp}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </article>
+              <ExperienceCard
+                key={exp.company}
+                exp={exp}
+                delayClasses={delayClasses}
+              />
             );
           })}
         </section>
