ed04c2393ad7baba5818653678423beaac301181
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-11-08 12:43:02 -0800

    starter landing. future: refactor illustrations into components for fluid interactivity

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,8 +1,123 @@
+<script lang="ts">
+    import A from "$lib/components/a.svelte";
+
+    import ArrowSquareOut from "$lib/icons/arrow-square-out.svelte";
+    import flowerl from "$lib/illustrations/flower-main-l.svg";
+    import flowerr from "$lib/illustrations/flower-main-r.svg";
+    import flower1 from "$lib/illustrations/flower-1.svg";
+
+    $: projects = [
+        {
+            title: "Unearth",
+            href: "https://unearth.news",
+            imgSrc: flower1,
+            tagline: "An ai-native news analysis platform.",
+            tech: "Typescript, React, Express, PostgreSQL, Vertex AI SDK, Google Cloud Platform, Docker, GitHub Actions",
+        },
+    ];
+</script>
+
 <svelte:head>
     <title>Cristian Dominguez</title>
     <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-    <h1 class="font-display text-4xl">Hello Svelte</h1>
-</section>
+<main class="flex flex-col gap-40 p-4">
+    <section>
+        <h1 class="font-display text-4xl">
+            Hello, I’m Cristian &mdash; I’m a Software Engineer
+        </h1>
+
+        <img
+            src={flowerl}
+            alt="An illustrated flower silhouette"
+            class="mx-auto my-16"
+        />
+
+        <p>
+            I’m mostly <span class="italic font-semibold"
+                >Typescript-adjacent</span
+            >
+            with a professional toolkit involving
+            <span class="font-semibold">Java, React, Node, and PostgreSQL</span
+            >.
+        </p>
+        <p class="py-3">
+            On the side, I’ve been messing around with C and Go while hunting
+            down the best matcha in <span class="font-semibold"
+                >San Diego, CA</span
+            >.
+        </p>
+        <p>
+            I’m big on clean abstractions, strong typing, and finding tech that
+            just
+            <span class="italic">feels right</span>.
+        </p>
+
+        <img
+            src={flowerr}
+            alt="An illustrated flower silhouette"
+            class="mx-auto my-16"
+        />
+
+        <p>
+            Connect With Me:
+            <A
+                href="https://linkedin.com/in/cdom27"
+                cn="hover:text-sky-600 active:text-sky-600"
+            >
+                Linkedin
+            </A>,
+            <A
+                href="https://github.com/cdom27"
+                cn="hover:text-indigo-600 active:text-indigo-600"
+            >
+                GitHub
+            </A>,
+            <A
+                href="mailto:cdom27@outlook.com"
+                cn="hover:text-rose-600 active:text-rose-600"
+            >
+                Email
+            </A>,
+            <A
+                href="/cristian-dominguez-resume.pdf"
+                cn="hover:text-emerald-600 active:text-emerald-600"
+                outbound
+            >
+                Resume
+            </A>
+        </p>
+    </section>
+
+    <section class="flex flex-col gap-16">
+        <h2 class="font-display text-3xl">Programming</h2>
+
+        <ul>
+            {#each projects as p}
+                <li>
+                    <A
+                        href={p.href}
+                        cn="group flex flex-col p-6 gap-6 border-2 border-fg-dark-500"
+                        variant="simple"
+                    >
+                        <h3
+                            class="flex items-center mx-auto gap-2 font-normal font-display text-2xl underline underline-offset-4"
+                        >
+                            <span>{p.title}</span><ArrowSquareOut
+                                className="size-6 stroke-fg-dark-500 border-none"
+                            />
+                        </h3>
+                        <img
+                            src={p.imgSrc}
+                            alt="A silhouette of a flower with two leaves at the stem"
+                            class="max-h-35"
+                        />
+                        <p class="font-semibold">{p.tagline}</p>
+                        <p class="font-medium pt-3">{p.tech}</p>
+                    </A>
+                </li>
+            {/each}
+        </ul>
+    </section>
+</main>
