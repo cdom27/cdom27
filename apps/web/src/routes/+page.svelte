ae217ef94ac5f37890628c0357f9b3c31964d6ae
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-11-07 21:49:04 -0800

    scaffold basic svelte app, update gitignore

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>Cristian Dominguez</title>
+    <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+    <h1 class="font-sans text-4xl">Hello Svelte</h1>
+</section>
