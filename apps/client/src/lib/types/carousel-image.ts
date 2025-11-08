973683609be1019b467d35de6329c6cdbc00c67e
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-11-07 20:49:45 -0800

    remove v1 apps

--- apps/client/src/lib/types/carousel-image.ts
+++
@@ -1,5 +0,0 @@
-export type CarouselImage = {
-  src: string;
-  alt: string;
-  caption: string;
-};
--- apps/client/src/lib/types/experience/personal-work.ts
+++
@@ -1,8 +0,0 @@
-export type PersonalWork = {
-  title: string;
-  image: string;
-  description: string;
-  stack: string[];
-  site_url: string;
-  repo_url: string;
-};
