e0a54049d6f5b5c65f7346d3f2ebc02880d2cd8f
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-01-24 22:32:32 -0800

    update imports

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,48 +2,7 @@ import Link from "next/link";
 import CellGrid from "./_components/ui/cell-grid";
 import Image from "next/image";
 import { projects } from "./_lib/utils/projects";
-import UNEARTH from "../public/unearth.png";
-
-const work = [
-  {
-    id: 0,
-    imgSrc: UNEARTH,
-    title: "Unearth News",
-    duration: "Sept. 2025 - Present",
-    description: `AI-powered news analysis with a mobile-first, accessible UI. Users
-    paste a URL to get a clear breakdown of claims/framing plus
-    related coverage and source credibility context.`,
-    tags: [
-      "TypeScript",
-      "React",
-      "Node.js / Express",
-      "PostrgeSQL",
-      "GCP",
-      "Docker",
-    ],
-    liveUrl: "https://unearth.news",
-    codebaseUrl: "https://github.com/cdom27/unearth_news",
-  },
-  {
-    id: 1,
-    imgSrc: null,
-    title: "Open Artwork",
-    duration: "May 2025 - Present",
-    description: `A developer-friendly public-domain art API with a lightweight,
-    responsive front-end for browsing clean metadata (artist, medium,
-    year) and optimized images.`,
-    tags: [
-      "TypeScript",
-      "React",
-      "Node.js / Express",
-      "PostrgeSQL",
-      "GCP",
-      "Docker",
-    ],
-    liveUrl: "https://openart.work",
-    codebaseUrl: "https://github.com/cdom27/art-api",
-  },
-];
+import { work } from "./_lib/utils/work";
 
 export default function Home() {
   return (
